917afe069d07ae1cab6b84bcb960f66f10a73d17
Qaiser Habib <121309452+qaiser1Habib@users.noreply.github.com>
2024-07-27 20:36:26 +0500

    fix: build

--- app/api/auth/[...nextauth]/option.ts
+++ app/api/auth/[...nextauth]/option.ts
@@ -1,5 +1,4 @@
 import bcrypt from "bcrypt";
-import NextAuth from "next-auth";
 import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
