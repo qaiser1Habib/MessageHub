25959fc6c6e825dfef81c53e3169fe1731d5bb47
Qaiser Habib <121309452+qaiser1Habib@users.noreply.github.com>
2024-07-27 19:51:33 +0500

    fix with as never

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/app/libs/prismadb";
 
-const authOptions: AuthOptions = {
+export const authOptions: AuthOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
 		GithubProvider({
@@ -37,26 +37,23 @@ const authOptions: AuthOptions = {
 					throw new Error("invalid credentials");
 				}
 
-        const isCorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
+				const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
 
-        if (!isCorrectPassword) {
-          throw new Error('Invalid credentials');
-        }
+				if (!isCorrectPassword) {
+					throw new Error("Invalid credentials");
+				}
 
-        return user;
-      }
-    })
-  ],
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+				return user;
+			},
+		}),
+	],
+	debug: process.env.NODE_ENV === "development",
+	session: {
+		strategy: "jwt",
+	},
+	secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions) as never;
 
 export { handler as GET, handler as POST };
