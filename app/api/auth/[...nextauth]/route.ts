817961db439acc9fbcd4e22e80d0d379b7f647f2
Qaiser Habib <121309452+qaiser1Habib@users.noreply.github.com>
2024-07-27 20:03:05 +0500

    fix from doc

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth"
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
